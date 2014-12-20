7344ac94c3d1edcc38f5c47b727d546af460711d
Kelly Boswell <KellyBoswell@solutionary.com>
2014-12-20 15:05:31 -0600

    Analysis seems to be working

--- create_tidy_data.R
+++
@@ -1,5 +0,0 @@
-create_tidy_data <- function(data) {
-    means <- sapply(data[complete.cases(data), ], function(x) mean(as.numeric(x)))[3:length(data)]
-    
-    means
-}
--- run_analysis.R
+++ run_analysis.R
@@ -17,23 +17,75 @@
 
 run_analysis <- function() {
     
-    #------------------------------------------------------------------------
-    # Local variables.
-    #------------------------------------------------------------------------
-    
-    # The location of the raw data.
-    raw_data_loc <- "data/raw"
-    
-    # The location of the tidy data.
-    tidy_data_loc <- "data/tidy"
+    require(dplyr)
     
     #------------------------------------------------------------------------
-    # Method invocations.
+    # 1. Merge the training and test sets to create one data set.
     #------------------------------------------------------------------------
     
-    # Read and merge data.
-    data <- merge_data_sets(raw_data_loc)
+    # Read the training data sets.
+    x_train <- read.table("data/raw/train/X_train.txt")
+    y_train <- read.table("data/raw/train/y_train.txt")
+    s_train <- read.table("data/raw/train/subject_train.txt")
     
-    # Create tidy data set.
-    create_tidy_data(data)
+    # Read the test data sets.
+    x_test <- read.table("data/raw/test/X_test.txt")
+    y_test <- read.table("data/raw/test/y_test.txt")
+    s_test <- read.table("data/raw/test/subject_test.txt")
+    
+    # Merge the training and test data sets together.
+    x_data <- rbind(x_train, x_test)
+    y_data <- rbind(y_train, y_test)
+    s_data <- rbind(s_train, s_test)
+    
+    #------------------------------------------------------------------------
+    # 2. Extract only the mean and standard deviation for each measurement.
+    #------------------------------------------------------------------------
+    
+    # Read the features vector.
+    all_features <- read.table("data/raw/features.txt")
+    
+    # Extract the mean and standard deviation feature column indexes.
+    feature_columns <- grep("-(mean|std)\\(\\)", all_features[, 2])
+    
+    # Subset the X data
+    x_data <- x_data[, feature_columns]
+    
+    # Set column names.
+    names(x_data) <- all_features[feature_columns, 2]
+    
+    #------------------------------------------------------------------------
+    # 3. Apply the activity labels to the y data set.
+    #------------------------------------------------------------------------
+    
+    # Read the activity labels data.
+    activities <- read.table("data/raw/activity_labels.txt")
+    
+    # Replace activity identifiers with equivalent labels.
+    y_data[, 1] <- activities[y_data[, 1], 2]
+    
+    names(y_data) <- "activity"
+
+    #------------------------------------------------------------------------
+    # 4. Finish labeling the data sets and bring all of them together into
+    #    a single data set.
+    #------------------------------------------------------------------------
+    
+    # Apply label to the subject data.
+    names(s_data) <- "subject"
+    
+    # Bring all of the data together into a single data set.
+    all_data <- cbind(x_data, y_data, s_data)
+    
+    #------------------------------------------------------------------------
+    # 5. Create a separate, tidy data set with the average of each variable
+    #    for each activity and each subject.
+    #------------------------------------------------------------------------
+    
+    all_df <- tbl_df(all_data)
+    averages_dplyr <- all_df %>% 
+        group_by(subject, activity) %>%
+        summarise_each(funs(mean))
+    
+    write.table(averages_dplyr, "data/tidy/averages_dplyr.txt", row.name=FALSE)
 }
